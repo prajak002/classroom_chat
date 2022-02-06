c4ceed678c66ba67bee71ca2c2558812a2ca37d7
PRAJAK SEN <80170713+prajak002@users.noreply.github.com>
2022-02-06 21:01:46 +0530

    Update channelContainer.jsx

--- client/src/component/channelContainer.jsx
+++ client/src/component/channelContainer.jsx
@@ -1,35 +1,11 @@
 import React from 'react';
-import { ChannelList,useChatContext } from 'stream-chat-react';
-import { ChannelSearch,TeamChannelList,TeamChannelPreview} from '.';
-import cookies from 'universal-cookie';
-import SideBar from './SideBar';
 
-const CompanyHeader =()=>(
- <div className="channel-list__header">
-     <p className="channel-list__header__text">V-BOOK/V-CLASSROOM</p>
-     
- </div>
-
-);
-const ChannelContainerlist = () => {
-  return( 
-  <div style={{display:'flex',flexDirection:'row'}}>
-     <SideBar/> 
-     <div className="channel-list__list__wrapper">
-         <CompanyHeader/>
-         <ChannelList 
-         filters={}
-         channelRenderFilterFn={}
-         List={(listProps)=>(<TeamChannelList
-            {...listProps}/>)}
-            />
-         
-            
-        
-     </div>
-     
+const ChannelContainer = () => {
+  return (
+  <div>
+    <h3>ChannelContainer</h3>
   </div>
   );
-}
+};
 
-export default ChannelContainerlist;
+export default ChannelContainer;
